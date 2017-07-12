7be39b49159fe3d7e299141d727459b624db584e
unknown <estebitan9309@gmail.com>
2017-07-12 10:30:23 -0500

    cambio mensaje de cancelar por confirmar

--- ejemplo.js
+++ ejemplo.js
@@ -2,6 +2,7 @@ alert("C mamó")
 if(confirm("¿Estás contento en clase?")){
 	//se cumple la condición
 	alert("Me caes bien");
+	alert("En realidad todo")
 } else {
 	//NO cumple la condición
 	alert("Entonces preguntale a él cómo hace las clase más divertida");
